5b42243858cf7f7ad35788c57928b84a3df03e44
Troy B <tbraman@optimum.net>
2026-07-08 17:24:58 -0400

    Added CRUD endpoints, CQRS and Mediator pattern in API

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +8,17 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController : ControllerBase
     {
+        // <summary>
+        // The IMediator instance used to send requests and publish notifications.
+        // </summary>
+        private IMediator? _mediator;
+
+        // <summary>
+        // Gets the IMediator instance from the request services (program.cs).   
+        // </summary>
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>() 
+        ?? throw new InvalidOperationException("IMediator service is not registered.");
+
+        
     }
 }
